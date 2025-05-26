34f015e1c3c9587cb87e0ddfe3f340bb402ebdbd
Israel Akinoso <akinosoissy@gmail.com>
2025-05-26 22:12:57 +0100

    Intial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + Vite + Tailwind
+
+This template provides a minimal setup to get React and Tailwind working in Vite with HMR and some ESLint rules.
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./styles/App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold">
+        Amal <span className="text-sky-500 underline">Initiative</span>
+      </h1>
+    </>
+  );
+}
+
+export default App;
