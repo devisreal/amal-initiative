5708a84365671189549409d3570cfaedd5d22fc5
Israel Akinoso <akinosoissy@gmail.com>
2025-07-13 23:24:47 +0100

    feat: include social media handles for team members

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -109,11 +109,36 @@ const Team = () => {
                   alt="Bilal Hamza"
                 />
 
-                <h1 className="mt-4 text-2xl font-semibold text-gray-700 capitalize">
-                  Bilal Hamza
-                </h1>
+                <div className="flex items-center justify-between">
+                  <div>
+                    <h1 className="mt-4 text-2xl font-semibold text-gray-700 capitalize">
+                      Bilal Hamza
+                    </h1>
 
-                <p className="mt-2 text-gray-500 capitalize">AMAL Founder</p>
+                    <p className="mt-2 text-gray-500 capitalize">
+                      AMAL Founder
+                    </p>
+                  </div>
+
+                  <a
+                    href="https://www.linkedin.com/in/bilal-hamzeh/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <svg
+                      width="24"
+                      height="24"
+                      xmlns="http://www.w3.org/2000/svg"
+                      preserveAspectRatio="xMidYMid"
+                      viewBox="0 0 256 256"
+                    >
+                      <path
+                        d="M218.123 218.127h-37.931v-59.403c0-14.165-.253-32.4-19.728-32.4-19.756 0-22.779 15.434-22.779 31.369v60.43h-37.93V95.967h36.413v16.694h.51a39.907 39.907 0 0 1 35.928-19.733c38.445 0 45.533 25.288 45.533 58.186l-.016 67.013ZM56.955 79.27c-12.157.002-22.014-9.852-22.016-22.009-.002-12.157 9.851-22.014 22.008-22.016 12.157-.003 22.014 9.851 22.016 22.008A22.013 22.013 0 0 1 56.955 79.27m18.966 138.858H37.95V95.967h37.97v122.16ZM237.033.018H18.89C8.58-.098.125 8.161-.001 18.471v219.053c.122 10.315 8.576 18.582 18.89 18.474h218.144c10.336.128 18.823-8.139 18.966-18.474V18.454c-.147-10.33-8.635-18.588-18.966-18.453"
+                        className="fill-sky-600"
+                      />
+                    </svg>
+                  </a>
+                </div>
               </div>
 
               <div>
@@ -123,13 +148,36 @@ const Team = () => {
                   alt="UK Ambassador & Researcher"
                 />
 
-                <h1 className="mt-4 text-2xl font-semibold text-gray-700 capitalize">
-                  Hamza Yousef
-                </h1>
+                <div className="flex items-center justify-between">
+                  <div>
+                    <h1 className="mt-4 text-2xl font-semibold text-gray-700 capitalize">
+                      Hamza Yusuf
+                    </h1>
 
-                <p className="mt-2 text-gray-500 capitalize">
-                  UK Ambassador & Researcher
-                </p>
+                    <p className="mt-2 text-gray-500 capitalize">
+                      UK Ambassador & Researcher
+                    </p>
+                  </div>
+
+                  <a
+                    href="https://x.com/Hamza_a96"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="20"
+                      fill="none"
+                      viewBox="0 0 1200 1227"
+                    >
+                      <path
+                        className="fill-sky-600"
+                        d="M714.163 519.284 1160.89 0h-105.86L667.137 450.887 357.328 0H0l468.492 681.821L0 1226.37h105.866l409.625-476.152 327.181 476.152H1200L714.137 519.284h.026ZM569.165 687.828l-47.468-67.894-377.686-540.24h162.604l304.797 435.991 47.468 67.894 396.2 566.721H892.476L569.165 687.854v-.026Z"
+                      />
+                    </svg>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
